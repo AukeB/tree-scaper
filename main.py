b23e853fc0f43e973602ca5cd5e63f5f427b88c7
AukeB <afbruinsma@gmail.com>
2026-01-17 23:28:16 +0200

    Updated

--- main.py
+++ main.py
@@ -26,29 +26,34 @@ if __name__ == "__main__":
     main()
 
 
-    """
-    TODO:
-    - Runtime parameters                                                        DONE
-        - Dark/light mode -> self.dark_mode: bool = False                       DONE
-            - color_palettes opsplitsen in light en dark color_palettes         DONE
-            - Wat meer verschillende dark mode color palettes                   
-    - Laatste layer grijs                                                       DONE
-        - Color palette's aanpassen (groter contrast)                           DONE
-        - ColorPallete variabele in config                                      DONE
-        - Aantal paletten uitbreiden                                            DONE
-    - Kijken waar de groootte van nodes van afhangt                             DONE
-        - inzoomen/uitzoomen implementeren                                      DONE
-        - Niet alleen font maar ook                                             DONE
-            - margins, border widths, spacings in between                       DONE
-            - aanpsseen op basis van zoom                                       DONE
-        - In- and uitzoomen optimaliseren (met positie van de muis meegenomen)  
-    - Refactoring                                                               
-        - Kijken of alles naar 2 classes kan -> measuring & draw class          
-        - Misschien nog meer                                                    
-    - Compact mode                                                              
-        Misschien de v_stacks_leafs en compact_mode mergen en dan zeggen dat    
-        v_stack_leafs compact_mode level 1 is en de compact_mode wordt          
-        compact_model level 2.                                                  
-    - Vertical alignment van dezelfde level nodes als er een node is            
-        met een groter aantal regels in de title of subtitle                    
-    """
+"""
+TODO:
+- Runtime parameters                                                        DONE
+    - Dark/light mode -> self.dark_mode: bool = False                       DONE
+        - color_palettes opsplitsen in light en dark color_palettes         DONE
+        - Wat meer verschillende dark mode color palettes                   
+- Laatste layer grijs                                                       DONE
+    - Color palette's aanpassen (groter contrast)                           DONE
+    - ColorPallete variabele in config                                      DONE
+    - Aantal paletten uitbreiden                                            DONE
+- Kijken waar de groootte van nodes van afhangt                             DONE
+    - inzoomen/uitzoomen implementeren                                      DONE
+    - Niet alleen font maar ook                                             DONE
+        - margins, border widths, spacings in between                       DONE
+        - aanpsseen op basis van zoom                                       DONE
+    - In- and uitzoomen optimaliseren (met positie van de muis meegenomen)  
+- Images in the README.md stoppen                                           
+- README.md uitbreiden met features                                         
+- Refactoring                                                               
+    - Kijken of alles naar 2 classes kan -> measuring & draw class          
+    - Misschien nog meer                                                    
+- Compact mode                                                              
+    Misschien de v_stacks_leafs en compact_mode mergen en dan zeggen dat    
+    v_stack_leafs compact_mode level 1 is en de compact_mode wordt          
+    compact_model level 2.                                                  
+- Vertical alignment van dezelfde level nodes als er een node is            
+    met een groter aantal regels in de title of subtitle                    
+- CLI                                                                       
+    - parameter in cli voor meegevben data_path naar .json file             
+    - Andere runtime parameters toevoegen                                   
+"""
